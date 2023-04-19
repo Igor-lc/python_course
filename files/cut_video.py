d55c07cb3accbfb7601fb6d1b623fbf4ad88891b
Igor-lc <105611667+Igor-lc@users.noreply.github.com>
2023-04-19 18:58:49 +0300

    Update cut_video.py

--- files/cut_video.py
+++ files/cut_video.py
@@ -3,7 +3,7 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 # Відкриваємо відеофайл
 clip = VideoFileClip("1.mp4")
 
-# Обрізаємо відео від 5 до 7 секунди
+# Обрізаємо відео від 5 до 15 секунди
 clip = clip.subclip(5, 15)
 
 # Зберігаємо відео в новому файлі

25974edac1572c0430d8eb3b17d11bbbad6a5c2c
Igor-lc <105611667+Igor-lc@users.noreply.github.com>
2023-04-21 18:55:05 +0300

    Add files via upload

--- functions/digital_root.py
+++ functions/digital_root.py
@@ -4,51 +4,53 @@ current_limit = sys.getrecursionlimit()
 print("Поточн максимальна глибина рекурсії:", current_limit)
 
 # I use recursion
+# ver 1
+def root1(number):
+    number = sum(map(int, list(str(number))))
+    return (number < 10 and number) or root1(number)
 
-def root(number):
+
+# ver 2
+def root2(number):
+    number = sum(map(int, list(str(number))))
+    return (root2, lambda x: x)[number < 10](number)
+
+
+# ver 3
+def root3(number):
+    number = sum(map(int, list(str(number))))
+    return (lambda x: x if x < 10 else root3(x))(number)
+
+
+# ver 4
+def root4(number):
+    number = sum(map(int, list(str(number))))
+    return (lambda x: root4(number), lambda x: number)[number < 10](number)
+
+
+# ver 5
+def root5(number):
     result = 0
     for i in str(number):
         result += int(i)
     if result < 10:
         return result
-    return root(result)
-print(root(95554651))
+    return root5(result)
 
-# ver2
-def root2(number):
+
+# ver 6
+def root6(number):
     result = 0
     while number > 0:
         result += number % 10
         number //= 10
     if result < 10:
         return result
-    return root2(result)
-print(root2(95554651))
-
-
-# ver3
-def root3(number):
-    number = sum(map(int, list(str(number))))
-    return (lambda x: root3(number), lambda x: number)[number < 10](number)
-print(root3("95554651"))
-
-
-# ver 4
-def root4(number):
-    number = sum(map(int, list(str(number))))
-    return (lambda x: x if x < 10 else root(x))(number)
-print(root4("95554651"))
-
-
-def root5(number):
-    number = sum(map(int, list(str(number))))
-    return (root5(number), lambda x: number)[number < 10]
-print(root5("95554651"))
-
+    return root6(result)
 
 
 # bad version:
-def root6(number):
+def root7(number):
     dr = 0
     for i in str(number):
         dr += int(i)
@@ -64,5 +66,11 @@ def root6(number):
     else:
         return dr
 
-print(root6(95554651))
+print(root1(9999999))
+print(root2(9999999))
+print(root3(9999999))
+print(root4(9999999))
+print(root5(9999999))
+print(root6(9999999))
+print(root7(9999999))
 

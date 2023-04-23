185df347223ca375648c74ce5b1164a636bbabe4
Igor-lc <105611667+Igor-lc@users.noreply.github.com>
2023-04-23 18:57:50 +0300

    Add files via upload

--- functions/dates.py
+++ functions/dates.py
@@ -1,6 +1,5 @@
 import datetime
-
-
+# ver 1
 def dates(date):
     d, m, y = date.split(".")
     yesterday = datetime.date(int(y), int(m), int(d)).replace(day=int(d) - 1).strftime("%d.%m.%Y")
@@ -8,9 +7,9 @@ def dates(date):
     return yesterday, tomorrow
 
 
-result = dates('12.03.2016')
-print(result)  # ('11.03.2016', '13.03.2016')
-
+# ver 2
+DAYS = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+DAYS2 = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def is_leap_year(year):
     if not year % 100:
@@ -19,14 +18,45 @@ def is_leap_year(year):
         return (year % 4) == 0
 
 
-def is_leap_year2(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
+def dates2(date):
+    day, month, year = date.split(".")
+    day, month, year = int(day), int(month), int(year)
+
+    if is_leap_year(year):
+        days = DAYS2
+    else:
+        days = DAYS
+
+    prev_day, prev_month, prev_year = day, month, year
+    if day == 1:
+        if month == 1:
+            prev_year = year - 1
+            prev_month = 12
         else:
-            return True
+            prev_month = month - 1
 
-    return False
+        prev_day = days[prev_month]
+    else:
+        prev_year = year
+        prev_month = month
+        prev_day = day - 1
 
-print(is_leap_year2(2017))
+    next_day, next_month, next_year = day, month, year
+    if day == days[month]:
+        if month == 12:
+            next_year = year + 1
+            next_month = 1
+        else:
+            next_month = month + 1
+        next_day = 1
+    else:
+        next_year = year
+        next_month = month
+        next_day = day + 1
+
+    prev_date = "{:02d}.{:02d}.{:02d}".format(prev_day, prev_month, prev_year)
+    next_date = "{:02d}.{:02d}.{:02d}".format(next_day, next_month, next_year)
+    return prev_date, next_date
+
+result = dates2('12.03.2016')
+print(result)

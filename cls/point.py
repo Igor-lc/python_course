06c239b1d3833a51ea4dfd38d3b4b47da4f0d6a8
Igor-lc <105611667+Igor-lc@users.noreply.github.com>
2023-05-04 15:04:24 +0300

    Add files via upload

--- cls/point.py
+++ cls/point.py
@@ -2,48 +2,35 @@ class Point:
     def __init__(self, x, y):
         self._x = float(x)
         self._y = float(y)
-    def set_x(self, x):
-        self._x = float(x)
+
     def get_x(self):
         return self._x
-    def set_y(self, y):
-        self.y = float(y)
+
     def get_y(self):
         return self._y
-    x = property(get_x, set_x)
-    y = property(get_y, set_y)
-
-'''class Point:
-    def __init__(self, x, y):
-        self._x = float(x)
-        self._y = float(y)
-
-    def get_x(self):
-        return self._x
 
     def set_x(self, value):
         self._x = float(value)
 
-    def get_y(self):
-        return self._y
-
     def set_y(self, value):
         self._y = float(value)
 
-    def set(self, x, y):
-        self.x = x
-        self.y = y
+    def set(self, v1, v2):
+        self._x = float(v1)
+        self._y = float(v2)
 
     x = property(get_x, set_x)
-    y = property(get_y, set_y)'''
+    y = property(get_y, set_y)
 
-point = Point(12.34, "15.46")
-print(type(point.x))
-print(type(point.y))
-point.x = "4"
-point.y = 5.4
-print(point.x)
-print(point.y)
-point.set(7, "99.8")
-print(point.x)
-print(type(point.y))
+
+'''320. DOT
+Difficulty: 2 out of 10
+Create a Point class that will model the behavior of a point in a rectangular coordinate system on a plane.
+
+The class constructor must take two arguments x and y, which are responsible for the coordinates of the point.
+
+Change of coordinates of a point has to happen by means of properties (property). Each coordinate must be a separate point property.
+
+We also need to add the set(x, y) method, which can be used to change both coordinates.
+
+Coordinate values must be stored as floats, however the class must accept coordinates represented as both numbers (int, float) and strings (str) that can be converted to float.'''

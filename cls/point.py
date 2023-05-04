938487bb9b496b0cd68ee93d1524939e961949a8
Igor-lc <105611667+Igor-lc@users.noreply.github.com>
2023-05-04 15:07:46 +0300

    Add files via upload

--- cls/point.py
+++ cls/point.py
@@ -1,26 +1,36 @@
 class Point:
     def __init__(self, x, y):
+        self._x = self._y = 0
+        self.set(x, y)
+
+    def set_x(self, x):
         self._x = float(x)
-        self._y = float(y)
 
     def get_x(self):
         return self._x
 
+    def set_y(self, y):
+        self._y = float(y)
+
     def get_y(self):
         return self._y
 
-    def set_x(self, value):
-        self._x = float(value)
-
-    def set_y(self, value):
-        self._y = float(value)
-
-    def set(self, v1, v2):
-        self._x = float(v1)
-        self._y = float(v2)
+    def set(self, x, y):
+        self.set_x(x)
+        self.set_y(y)
 
     x = property(get_x, set_x)
     y = property(get_y, set_y)
+
+point = Point(12.34, "15.46")
+print(type(point.y))
+point.x = "4"
+point.y = 5.4
+print(point.x)
+print(point.y)
+point.set(7, "99.8")
+print(point.x)
+print(point.y)
 
 
 '''320. DOT

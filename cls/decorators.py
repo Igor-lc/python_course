0e975d4d790080a0bb52632e98e9c25ce68168c9
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-07-14 15:49:00 +0300

    Add files via upload

---
+++ cls/decorators.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+def not_during_the_night(func):
+    def wrapper():
+        if 7 <= datetime.now().hour < 22:
+            func()
+        else:
+            pass  # Hush, the neighbors are asleep
+    return wrapper
+
+def say_whee():
+    print("Whee!")
+
+say_whee = not_during_the_night(say_whee)
+say_whee()
+
+
+import functools
+
+def decorator(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        # Do something before
+        value = func(*args, **kwargs)
+        # Do something after
+        return value
+    return wrapper_decorator
+
+

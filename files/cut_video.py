1ae0837963f258623f6b99cb4156f49280bd4d5b
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-11-08 10:54:41 +0200

    Add files via upload

--- files/cut_video.py
+++ files/cut_video.py
@@ -1,10 +1,61 @@
+# Імпортуємо бібліотеку
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 # Відкриваємо відеофайл
 clip = VideoFileClip("1.mp4")
 
-# Обрізаємо відео від 5 до 15 секунди
-clip = clip.subclip(5, 15)
+# Обрізаємо відео від 3 до 7 секунди
+clip = clip.subclip(0, 1657)
 
 # Зберігаємо відео в новому файлі
-clip.write_videofile("example_trimmed.mp4")
+clip.write_videofile("11.mp4")
+
+
+
+"""
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+
+# Open the video file
+video_file = "1.mp4"
+clip = VideoFileClip(video_file)
+
+# Set the start and end times for the part you want to cut
+start_time = 32  # Start time in seconds
+end_time = 49   # End time in seconds
+
+# Cut the video from start_time to end_time
+subclip = clip.subclip(start_time, end_time)
+
+# Save the cut video to a new file
+subclip.write_videofile("11.mp4")
+"""
+
+
+
+
+'''
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import concatenate_videoclips
+
+# Open the video file
+video_file = "6.mp4"
+clip = VideoFileClip(video_file)
+
+# Set the start and end times for the parts you want to keep
+start_time_1 = 0       # Start of the video to keep
+end_time_1 = 53.4        # End of the first part
+start_time_2 = 53.47     # Start of the second part
+end_time_2 = clip.duration  # End of the video to keep
+
+# Extract the two parts you want to keep
+part1 = clip.subclip(start_time_1, end_time_1)
+part2 = clip.subclip(start_time_2, end_time_2)
+
+# Concatenate the two parts to create the final video
+final_clip = concatenate_videoclips([part1, part2])
+
+# Save the final video to a new file
+final_clip.write_videofile("7.mp4")
+'''
